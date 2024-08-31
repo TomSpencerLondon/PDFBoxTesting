b98304849508f8ad67a22feb0eb0493be4d4dad8
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2024-08-31 13:41:37 +0100

    Add save for document

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Path resourcePath = Paths.get("src", "main", "resources", "myDocument.pdf");
+
+        PDDocument document = new PDDocument();
+
+        document.save(resourcePath.toString());
     }
 }
