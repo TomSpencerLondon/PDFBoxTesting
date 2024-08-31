7ab682711531fe16f058c40530506e24fc80d23e
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2024-08-31 13:46:34 +0100

    Add page to document

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,9 +11,12 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) throws IOException {
         Path resourcePath = Paths.get("src", "main", "resources", "myDocument.pdf");
-
         PDDocument document = new PDDocument();
+        PDPage firstPage = new PDPage();
+        document.addPage(firstPage);
 
         document.save(resourcePath.toString());
+        System.out.println("PDF created");
+        document.close();
     }
 }
